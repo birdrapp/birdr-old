b6333baafc0233636e764516864fbead27aaef74
Robin Murphy <robin@robinmurphy.co.uk>
2017-08-26 14:50:28 +0100

    Add validation of name and description

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,10 @@
 class Club < ApplicationRecord
   has_many :club_memberships
   has_many :users, through: :club_memberships
+  validates :name, :presence => true
+  validates :description, :presence => true
+
+  def display_name
+    short_name ? short_name : name
+  end
 end
