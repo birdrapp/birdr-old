b3525c1284f6ff3645dfdaf47ba387b587dbb7c6
Robin Murphy <robin@robinmurphy.co.uk>
2017-08-26 14:01:35 +0100

    Add club scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824195022) do
+ActiveRecord::Schema.define(version: 20170826125835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20170824195022) do
     t.datetime "updated_at", null: false
     t.index ["sort_position"], name: "index_birds_on_sort_position"
     t.index ["species_id"], name: "index_birds_on_species_id"
+  end
+
+  create_table "clubs", force: :cascade do |t|
+    t.string "name"
+    t.string "short_name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
