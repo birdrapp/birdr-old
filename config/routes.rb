80e646d07dd5486872d136b8f814f6e779ad31fe
Matthew Williams <matthew.williams@me.com>
2017-08-26 21:23:04 +0100

    Update sign in pages, such heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :birds
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
   }
 
   root to: 'welcome#index'
