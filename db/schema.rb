d37058ac5c85b94193927d76470e55131d37e452
Matthew Williams <matthew.williams@me.com>
2017-08-27 22:20:31 +0100

    Enable postgis

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826130438) do
+ActiveRecord::Schema.define(version: 20170827211918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "birds", force: :cascade do |t|
     t.string "common_name"
