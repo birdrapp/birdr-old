12c9017f13fb3e624dbca0d10ad9d8721e2c73fa
Matthew Williams <matthew.williams@me.com>
2017-08-27 20:43:54 +0100

    Create account settings page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  layout "sign_in"
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
+  layout "sign_in", except: [:edit, :update]
 
   # GET /resource/sign_up
   # def new
@@ -45,9 +45,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
