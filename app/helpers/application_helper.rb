1aa78d35181f67f42135bb801b623fa6b0cc9734
Matthew Williams <matthew.williams@me.com>
2017-08-28 07:20:43 +0100

    Add horizontal_rule helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def nav_link_class path
     request.path == path ? 'active nav-link' : 'nav-link'
   end
+
+  def horizontal_rule
+    tag(:hr, class: 'my-5')
+  end
 end
