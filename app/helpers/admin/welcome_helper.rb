7e325f081f59c2a92006ece18a1c3165f29d9520
Matthew Williams <matthew.williams@me.com>
2017-09-02 20:42:12 +0100

    Create a helper for admin menu

---
+++ app/helpers/admin/welcome_helper.rb
@@ -0,0 +1,8 @@
+module Admin::WelcomeHelper
+  def admin_menu_link(model)
+    link_to admin_birds_path, class: 'list-group-item d-flex justify-content-between align-items-center' do
+      concat model.name.pluralize.titleize
+      concat content_tag(:span, number_with_delimiter(model.count), class: 'badge badge-dark')
+    end
+  end
+end
