d23603c88f34eb865be407e043d456e9d0261823
Matthew Williams <matthew.williams@me.com>
2017-09-03 12:08:22 +0100

    Added flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
   def horizontal_rule
     tag(:hr, class: 'my-5')
   end
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show") do
+        concat(content_tag(:button, class: "close", data: { dismiss: 'alert' }) do
+          content_tag(:span, '&times;'.html_safe)
+        end)
+        concat message
+      end)
+    end
+    nil
+  end
 end
