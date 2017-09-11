530874686fd721a22452cde1528d691ac69ad117
Robin Murphy <robin@robinmurphy.co.uk>
2017-09-11 21:16:07 +0100

    Add basic Google map

--- app/javascript/clubs/SetRecordingArea.js
+++ app/javascript/clubs/SetRecordingArea.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const defaultLocation = { lat: 51.505, lng: -0.09 };
+const defaultZoom = 10;
+const apiKey = 'AIzaSyCC3Ebzxe2VKuB54kd9baaW-7ztMxyRDA4';
 
 const SetRecordingArea = () => {
-  return <p>A map</p>;
+  return (
+    <div style={{ width: '100%', height: '500px' }}>
+      <GoogleMapReact
+        defaultCenter={defaultLocation}
+        defaultZoom={defaultZoom}
+        bootstrapURLKeys={{ key: apiKey }}
+      />
+    </div>
+  );
 };
 
 export default SetRecordingArea;
