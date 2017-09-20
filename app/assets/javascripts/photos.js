ba7a99ffa578fb7b8e8a487502e316a63d62451a
Matthew Williams <matthew.williams@me.com>
2017-09-20 07:24:16 +0100

    Fix ES6 issue with Heroku

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -9,12 +9,5 @@ Dropzone.options.newPhoto = {
   thumbnailWidth: 180,
   thumbnailHeight: 180,
   maxFiles: 3,
-  previewTemplate: `
-  <div class="card dz-preview dz-file-preview mb-4">
-    <img data-dz-thumbnail class="card-img-top" />
-    <div class="progress dz-upload">
-      <div class="progress-bar dz-upload" role="progressbar" data-dz-uploadprogress></div>
-    </div>
-  </div>
-  `
+  previewTemplate: '<div class="card dz-preview dz-file-preview mb-4"><img data-dz-thumbnail class="card-img-top" /><div class="progress dz-upload"><div class="progress-bar dz-upload" role="progressbar" data-dz-uploadprogress></div></div></div>'
 };
