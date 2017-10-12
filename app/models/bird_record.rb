097b3c27047e4a9910ed9947fb91e0d2f5740c4e
Matthew Williams <matthew.williams@me.com>
2017-10-12 20:51:23 +0100

    Add properties to bird session/record models

--- app/models/bird_record.rb
+++ app/models/bird_record.rb
@@ -24,6 +24,10 @@ class BirdRecord < ApplicationRecord
   scope :kilometres_from_session, -> (kilometres, location) { joins(:birding_session).where('ST_DWithin(birding_sessions.location, Geography(ST_MakePoint(?, ?)), ?)', location.lon, location.lat, kilometres * 1000) }
   scope :kilometres_from_record, -> (kilometres, location) { joins(:birding_session).where('ST_DWithin(bird_records.location, Geography(ST_MakePoint(?, ?)), ?)', location.lon, location.lat, kilometres * 1000) }
 
+  def bird_name
+    bird.common_name
+  end
+
   def date
     birding_session.date
   end
